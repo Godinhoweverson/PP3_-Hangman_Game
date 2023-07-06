5400c5535879230ad54773caa7f0ba47b4f908b0
Godinhoweverson <>
2023-07-06 21:52:14 +0000

    fix the errors of indented for visual indent

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-import random # Import the 'random' module 
-# to use later for selecting a random country.
-
+import random
+"""Import the 'random' module
+to use later for selecting a random country."""
 
 # Importing the 'american_countries' module.
 import american_countries
@@ -23,8 +23,13 @@ def choice_continent():
     # Function to get the user's choice for a continent.
     while True:
         try:
-            # Use 'input' to get user input and 'int' to convert it to an integer.
-            choice_number = int(input("Please select one continent from the following options: \n\n 1: American \n 2: Asian \n 3: Europe \n\n Please enter a number between 1 and 3: "))
+            """Use 'input' to get user input and
+             'int' to convert it to an integer."""
+            choice_number = int(input("Please select one continent from the following options: \n\n"
+                         " 1: American \n"
+                         " 2: Asian \n"
+                         " 3: Europe \n\n"
+                         " Please enter a number between 1 and 3: "))
             # Check if the input is within the valid range (1 to 3).
             if choice_number >= 1 and choice_number <= 3:
 
@@ -33,30 +38,28 @@ def choice_continent():
 
             else:
                 # If the input is not valid, inform the user and prompt again.
-                print('Ops! That was not a valid number. try again\n\n')     
+                print('Ops! That was not a valid number. try again\n\n')
         except ValueError:
             print('Please enter a number between 1 and 3.\n\n')
     # Return the valid choice number.
     return choice_number
 
 
-# Call the function to get the user's choice and store it in 'option_number'.   
+# Call the function to get the user's choice and store it in 'option_number'.
 option_number = choice_continent()
 
 
 def generate_word(number):
-    # Function to generate a random country name based on the selected continent number.
-           
-    # If the number is 1, choose a random country from 
-    # the 'american_countries' list and return it.
+    """Function to generate a random country name
+    based on the selected continent number.
+    If the number is 1, choose a random country from
+    the 'american_countries' list and return it."""
     if number == 1:
         return random.choice(american_countries.american_countries).upper()
-       
-    # If the number is 2, choose a random country from the 'asian_countries' list and return it.
+
     elif number == 2:
         return random.choice(asian_countries.asian_countries).upper()
-    
-    # If the number is 3, choose a random country from the 'european_countries' list and return it.
+
     elif number == 3:
         return random.choice(european_countries.european_countries).upper()
 
@@ -66,22 +69,22 @@ word = generate_word(option_number)
 
 
 def get_single_letter(letter):
-#This function enables the player to type only one letter at a time. 
-# If the player attempts to type two or more letters, they will not be able to proceed until they have typed a single letter. 
-# This function can be called within the check_letter()
-      while True:
+    """ This function enables the player to type only one letter at a time.
+    If the player attempts to type two or more letters, they will not be able
+    to proceed until they have typed a single letter.
+    This function can be called within the check_letter()"""
+    while True:
         try:
             letter = input('Please enter a letter: \n').upper()
             if len(letter) == 1:
-                break 
+                break
             else:
-               
-                print('Please enter one letter at a time. Try again!\n\n')     
+
+                print('Please enter one letter at a time. Try again!\n\n')
         except ValueError:
             print('type one letter.\n\n')
-    
-    
-      return letter
+    return letter
+
 
 def play_again():
     while True:
@@ -101,13 +104,20 @@ def play_again():
         except ValueError:
             print('Please, Y or N\n\n')
 
-def check_letter(word):
-    correct_letter_count = 0  # Counter for the number of correctly guessed letters
-    wrong_letter_count = 6  # Counter for the number of incorrectly guessed letters
-    correct = ''  # String to store correctly guessed letters
-    wrong = ''  # String to store incorrectly guessed letters
 
-    while correct_letter_count != len(word.replace(' ', '')) and wrong_letter_count != 0:  # Continue until all letters are guessed correctly or 6 wrong guesses are made
+def check_letter(word):
+    # Counter for the number of correctly guessed letters
+    correct_letter_count = 0
+    # Counter for the number of incorrectly guessed letters
+    wrong_letter_count = 6
+    # String to store correctly guessed letters
+    correct = ''
+    # String to store incorrectly guessed letters
+    wrong = ''
+
+    """Continue until all letters are guessed
+    correctly or 6 wrong guesses are made"""
+    while correct_letter_count != len(word.replace(' ', '')) and wrong_letter_count != 0:
         display = ''
         for letter in word:
             if letter in correct:
@@ -115,7 +125,7 @@ def check_letter(word):
             else:
                 display += '_ '  # Display underscores for unguessed letters
         print(display)
-       
+
         letter = get_single_letter(letter)
 
         if letter in correct + wrong:
@@ -126,26 +136,29 @@ def check_letter(word):
 
         if letter in word:
             # The guessed letter is in the word
-            print('You got the letter right!')  
-             # Add the correctly guessed letter to the 'correct' string
-            correct += letter 
+            print('You got the letter right!')
+            # Add the correctly guessed letter to the 'correct' string
+            correct += letter
             # Display the correctly guessed letters
-            print(f"You have entered the following letters:  {correct + wrong}\n\n")  
+            print("You have entered the following letters: \n\n"
+            f"{correct + wrong}\n\n")
+
             # Increment the count of correct guesses
-            correct_letter_count += word.count(letter)  
+            correct_letter_count += word.count(letter)
             if correct_letter_count == len(word.replace(' ', '')):
-                print( "You won the game! Congratulations!\n")
+                print("You won the game! Congratulations!\n")
                 print(f"The country is {word}\n\n")
                 play_again()
-    
+
         else:
             # The guessed letter is not in the word
-            print('You got the letter wrong!')  
-             # Add the incorrectly guessed letter to the 'wrong' string
-            wrong += letter 
-            print(f"You have entered the following letters:  {correct + wrong}\n\n")
-             # Increment the count of wrong guesses
-            wrong_letter_count -= 1 
+            print('You got the letter wrong!')
+            # Add the incorrectly guessed letter to the 'wrong' string
+            wrong += letter
+            print("You have entered the following letters: \n\n"
+            f"{correct + wrong}\n\n")
+            # Increment the count of wrong guesses
+            wrong_letter_count -= 1
             print(hangman_stages.stages[wrong_letter_count])
             if wrong_letter_count == 0:
                 print(f"The country was {word}\n\n")
@@ -153,12 +166,7 @@ def check_letter(word):
                 play_again()
 
 
-check_letter(word)  
-# To call the function, the parameter "word" is used. 
-# The word is a randomly selected country defined by the generate_word function.
-
-
-
-
-
-    
+check_letter(word)
+# To call the function, the parameter "word" is used.
+# The word is a randomly selected
+# country defined by the generate_word function.
